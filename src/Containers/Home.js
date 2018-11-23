944eb5f25d7d2ae582b6f7021196d1aa9061a689
Riku Ruokonen <riku.ruokonen@sujuwa.fi>
2018-11-24 00:27:42 +0200

    Forgot stuff

---
+++ src/Containers/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h2 className={'header'}>
+                        eKotutka
+                    </h2>
+                    <p>put charts etc here</p>
+
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/Containers/User.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h2 className={'header'}>
+                        Blank page
+                    </h2>
+                    <p>Put something here </p>
+
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Home;
