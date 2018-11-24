4f0185ba6124ca69d1aa0a9046773e991c0a54b0
Aapeli Haanpuu <aapzu@iki.fi>
2018-11-25 00:48:15 +0200

    Challenges cards

--- src/Containers/Challenges.js
+++ src/Containers/Challenges.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { getChallenges, getChallengeResults } from '../helpers/utils'
-import Header from './Header'
+import { RadialChart } from 'react-vis'
 import Collapsible from 'react-collapsible'
 
 class Challenges extends Component {
@@ -9,35 +9,43 @@ class Challenges extends Component {
         super()
         this.state = {
             challenges: [{
-                id: 'foo',
-                title: 'Vegan-Week',
-                description: '',
+                id: '5bf9b6c38673062e3af5efdf',
+                name: 'Domestic for the win',
+                description: '75% of your purchases are made in Finland',
+                goal: 75,
             }, {
-                id: 'bar',
-                title: 'Month Straight Local Food',
-                description: '',
-            }, {
-                id: 'bah',
-                title: '3 months without plastic bags',
-                description: '',
+                id: '5bf9b7ab82f24c2e93e2e77a',
+                name: 'Meatless month',
+                description: 'Don\'t buy any meat products this month',
+                goal: 30,
             }],
-            results: [],
+            results: [{
+                challenge: {
+                    id: '5bf9b7ab82f24c2e93e2e77a',
+                },
+                value: 45,
+            }, {
+                challenge: {
+                    id: '5bf9b6c38673062e3af5efdf',
+                },
+                value: 30,
+            }],
         }
     }
 
     componentDidMount() {
-        getChallenges()
-            .then(({ data }) => {
-                this.setState({
-                    challenges: data,
-                })
-            })
-        getChallengeResults()
-            .then(({ data }) => {
-                this.setState({
-                    results: data,
-                })
-            })
+        // getChallenges()
+        //     .then(({ data }) => {
+        //         this.setState({
+        //             challenges: data,
+        //         })
+        //     })
+        // getChallengeResults()
+        //     .then(({ data }) => {
+        //         this.setState({
+        //             results: data,
+        //         })
+        //     })
     }
 
     render() {
@@ -45,19 +53,45 @@ class Challenges extends Component {
         return (
             <div className="App">
                 <div className="content">
-                    {console.log(challenges)}
+                    <h3>Challenge yourself!</h3>
                     <p>Guidelines and challenges to track your environmental process</p>
-                    {results.map((c) => (
-                        <div>
-                        <Collapsible trigger={c.name}>
-                            <p>Lorem ipsum dolor sit amet</p>
-                        </Collapsible>
-                        </div>
-                    ))}
+                    <div className="Challenge_container">
+                        {challenges.map((c) => {
+                            const result = results.find((r) => r.challenge.id === c.id)
+                            const resultValue = (result && result.value) || 0
+                            const progress = resultValue / c.goal
+                            return (
+                                <div key={c.id} className="card Challenge_card">
+                                    <Collapsible
+                                        trigger={(
+                                            <p className="Challenge_title">
+                                                {c.name}
+                                                {progress >= 0.99 && (
+                                                    <div className="Challenge_card_checkmark_container">
+                                                        <img src={require('../resources/greencheck.png')} />
+                                                    </div>
+                                                )}
+                                                {progress > 0 && progress < 1 && (
+                                                    <div className="Challenge_card_progress_container">
+                                                        <img src={require('../resources/Pacman.svg')} />
+                                                    </div>
+                                                )}
+                                            </p>
+                                        )} transitionTime={200}
+                                    >
+                                        <p>{c.description}</p>
+                                        <p className="Challenge_card_footer">
+                                            Progress: {(progress * 100).toFixed(0)}%
+                                        </p>
+                                    </Collapsible>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
-        );
+        )
     }
 }
 
-export default Challenges;
+export default Challenges
